2285327030448afcf5ffab0a6dfb0f81ebeb22b2
Ghoulouis <maikhanh3241@gmail.com>
2024-05-03 14:59:44 +0700

    update: add deterministicDeployment

--- src/deploy/03_deploy_sa_implementation.ts
+++ src/deploy/03_deploy_sa_implementation.ts
@@ -3,20 +3,20 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { getEntryPoint } from "../utils/setupHelper";
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployments, getNamedAccounts } = hre;
-    const { deploy } = deployments;
-    const { deployer } = await getNamedAccounts();
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-    const entryPoint = await getEntryPoint();
+  const entryPoint = await getEntryPoint();
 
-    await deploy("SmartAccount", {
-        from: deployer,
-        args: [entryPoint.address],
-        log: true,
-        skipIfAlreadyDeployed: true,
-        // deterministicDeployment: true,
-        autoMine: true,
-    });
+  await deploy("SmartAccount", {
+    from: deployer,
+    args: [entryPoint.address],
+    log: true,
+    deterministicDeployment: true,
+    //skipIfAlreadyDeployed: true,
+    autoMine: true,
+  });
 };
 
 deploy.tags = ["local", "sapphire-testnet", "sapphire-localnet"];
--- src/deploy/07_deploy_mock_luminexRouter.ts
+++ src/deploy/07_deploy_mock_luminexRouter.ts
@@ -3,20 +3,19 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { getMockWrappedNative } from "../utils/setupHelper";
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployments, getNamedAccounts } = hre;
-    const { deploy } = deployments;
-    const { deployer } = await getNamedAccounts();
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const wrappedNative = await getMockWrappedNative();
 
-    const wrappedNative = await getMockWrappedNative();
-
-    await deploy("MockLuminexRouterV1", {
-        from: deployer,
-        args: [wrappedNative.address],
-        log: true,
-        //deterministicDeployment: true,
-        skipIfAlreadyDeployed: true,
-        autoMine: true,
-    });
+  await deploy("MockLuminexRouterV1", {
+    from: deployer,
+    args: [wrappedNative.address],
+    log: true,
+    //deterministicDeployment: true,
+    skipIfAlreadyDeployed: true,
+    autoMine: true,
+  });
 };
 
 deploy.tags = ["hardhat", "sapphire-localnet", "sapphire-testnet"];
